f3856363eaa4c975021ea457e6add51fc095549e
dsisnero <dsisnero@gmail.com>
2023-03-14 16:15:58 -0600

    add astrocommunity packs

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,14 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.editing-support.neogen.neogen" },
+  { import = "astrocommunity.pack.cmake.cmake" },
+  { import = "astrocommunity.pack.json.json" },
+  { import = "astrocommunity.pack.go.go" },
+  { import = "astrocommunity.pack.lua.lua" },
+  { import = "astrocommunity.pack.markdown.markdown" },
+  { import = "astrocommunity.pack.python.python" },
+  { import = "astrocommunity.pack.rust.rust" },
+  { import = "astrocommunity.pack.typescript.typescript" },
+  { import = "astrocommunity.pack.yaml.yaml" },
 }
