8a6c461c74eac5650cc3bd2f103c21a5be420767
dsisnero <dsisnero@gmail.com>
2023-03-14 17:28:18 -0600

    add yaml schemas

---
+++ lsp/config/yamlls.lua
@@ -0,0 +1,13 @@
+-- example for addings schemas to yamlls
+return {
+         -- override table for require("lspconfig").yamlls.setup({...})
+  settings = {
+    yaml = {
+      schemas = {
+        ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
+        ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+        ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+      },
+    },
+  },
+}
